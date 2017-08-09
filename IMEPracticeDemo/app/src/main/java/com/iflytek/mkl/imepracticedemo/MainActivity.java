ac3b4a38f0a36ec22a854e3986f18fabfc3c5b2e
makunle <makunle@qq.com>
2017-08-09 11:40:07 +0800

    meizu Sms get problem

--- IMEPracticeDemo/app/src/main/java/com/iflytek/mkl/imepracticedemo/MainActivity.java
+++ IMEPracticeDemo/app/src/main/java/com/iflytek/mkl/imepracticedemo/MainActivity.java
@@ -1,13 +1,64 @@
 package com.iflytek.mkl.imepracticedemo;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.permission_get).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getReadSMSPermission();
+            }
+        });
+    }
+
+    private boolean getReadSMSPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, REQUEST_CODE);
+            return false;
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, REQUEST_CODE);
+            return false;
+        }
+
+        Intent localIntent = new Intent();
+        localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (Build.VERSION.SDK_INT >= 9) {
+            localIntent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+            localIntent.setData(Uri.fromParts("package", getPackageName(), null));
+        } else if (Build.VERSION.SDK_INT <= 8) {
+            localIntent.setAction(Intent.ACTION_VIEW);
+            localIntent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
+            localIntent.putExtra("com.android.settings.ApplicationPkgName", getPackageName());
+        }
+        startActivity(localIntent);
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String[] permissions,  int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "can't get permission", Toast.LENGTH_SHORT).show();
+            } else {
+
+            }
+        }
     }
 }
--- IMEPracticeDemo/app/src/main/java/com/iflytek/mkl/imepracticedemo/SmsReceiveObserver.java
+++ IMEPracticeDemo/app/src/main/java/com/iflytek/mkl/imepracticedemo/SmsReceiveObserver.java
@@ -6,12 +6,16 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2017/8/9.
  */
 
 public class SmsReceiveObserver extends ContentObserver {
+
+    private static final String TAG = "SmsReceiveObserver";
 
     private Context context;
     private Handler handler;
@@ -24,11 +28,14 @@ public class SmsReceiveObserver extends ContentObserver {
 
     @Override
     public void onChange(boolean selfChange) {
-        Cursor cursor = context.getContentResolver().query(
-                Uri.parse("content://sms/inbox"), null, null, null, null
-        );
+
+        Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/"),
+                new String[]{"body"}, null, null, "_id desc");
         cursor.moveToNext();
         String body = cursor.getString(cursor.getColumnIndex("body"));
+
+        Toast.makeText(context, "observer msg:" + body + " code:" + VerificationCodeGetter.getCode(body), Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "observer msg:" + body + " code:" + VerificationCodeGetter.getCode(body));
 
         Message msg = new Message();
         msg.what = AndroidInputMethodService.IS_CODE;
